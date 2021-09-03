4e5cf0b11b5c0f251883904ab74b4c79c2429e85
Ctech Didik <89949173+CTechDidik@users.noreply.github.com>
2021-09-03 22:11:07 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,6 +26,33 @@ echo '                Tunggu 5 Saat!              '
 echo ' .................................... '
 sleep 5
 clear
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/ipdibenarkan | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Sila Hubungi Pihak CTech untuk mendapatkan kebenaran"
+echo "Telegram t.me/ctechdidik"
+echo "WhatsApp wa.me/00"
+rm -f setup.sh
+exit 0
+fi
+if [ -f "/etc/v2ray/domain" ]; then
+echo "Script Already Installed"
+exit 0
+fi
 echo '============================================='
 echo '          Sila Tunggu sebentar '
 echo 'Process Update & Upgrade Sedang Dijalankan '
