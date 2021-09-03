d3ec89a1325f39277f0befa2d0333553ae1cdc9a
Ctech Didik <89949173+CTechDidik@users.noreply.github.com>
2021-09-03 21:49:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl http://111.111.111.11/vpnmit/ipdibenarkan | grep $MYIP )
+IZIN=$( curl http://111.111.111.111/vpnmit/ipdibenarkan | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
