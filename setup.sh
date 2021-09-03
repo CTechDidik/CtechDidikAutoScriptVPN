5e629bccc84749d2beea046d8cc64c82eafdd322
Ctech Didik <89949173+CTechDidik@users.noreply.github.com>
2021-09-03 21:45:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,9 +34,9 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		echo "OpenVZ is not supported"
 		exit 1
 fi
-red="\[\033[0;31m\]"  
-green="\[\033[0;32m\]" 
-NC="\[\033[0;35m\]" 
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl http://111.111.111.11/vpnmit/ipdibenarkan | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
