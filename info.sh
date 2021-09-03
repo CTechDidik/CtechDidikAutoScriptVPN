33ec5c179616a5fa0f6bc7854c34a43b5050774c
Ctech Didik <89949173+CTechDidik@users.noreply.github.com>
2021-09-04 02:15:22 +0800

    Update info.sh

--- info.sh
+++ info.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 clear
-neofetch
+neofetch --ascii_distro Minix
+echo 'Created by CTechDidik'
+echo 'SamVPN'
+echo 'Youtube: CTech Didik'
+echo 'Telegram: @sam_sfx'
+echo ''
+echo ''
+echo 'Sila type menu'
 cat /root/log-install.txt
 echo ""
