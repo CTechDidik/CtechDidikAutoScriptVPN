7a97c98cda27f893d67d976fc1c0315ddc8af297
Ctech Didik <89949173+CTechDidik@users.noreply.github.com>
2021-09-03 21:54:18 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,4 @@
 clear
-#!/bin/bash
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl http://x.x.x.x/vpnmit/ipdibenarkan | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-echo "Telegram t.me/vpnmit"
-echo "WhatsApp wa.me/00"
-rm -f setup.sh
-exit 0
-fi
-if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
-exit 0
-fi
 echo ''
 echo '  ____ _____ _____ ____ _   _      ____ ___ ____ ___ _  __'
 echo ' / ___|_   _| ____/ ___| | | |    |  _ \_ _|  _ \_ _| |/ /'
