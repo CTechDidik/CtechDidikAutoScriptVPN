8dce88e50075faf2509b7ac0a5d96fee83e4ca9e
Ctech Didik <89949173+CTechDidik@users.noreply.github.com>
2021-09-08 17:16:28 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -142,7 +142,7 @@ echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 01.00 UTC +8" | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 UTC +8" | tee -a log-install.txt
 echo "   - Autobackup Data" | tee -a log-install.txt
 echo "   - Restore Data" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
